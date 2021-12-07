cb1ac688d1803c1128c5efe3463a97fa73bd2bdd
LittleDyingDuck <51399074+LittleDyingDuck@users.noreply.github.com>
2021-12-07 10:39:59 +1000

    Update Generate.php

--- Generate.php
+++ Generate.php
@@ -6,20 +6,24 @@ $Connection = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
 $USER_IP = $_SERVER['REMOTE_ADDR'];
 
-$Query = $Connection->query("SELECT * FROM `userdata` WHERE `USERIP`='$USER_IP'");
+$Sanitized1 = sprintf("SELECT * FROM `userdata` WHERE `USERIP`='%S'", $USER_IP);
+
+$Query = $Connection->query($Sanitized1);
 $Row = mysqli_fetch_row($Query);
 
 if(isset($_GET['solve'])) {
     if($Row) {
         if($Row[2] == 0) {
-            $Connection->query("UPDATE `userdata` SET `USERPASS`=1 WHERE `USERIP`='$USER_IP'");
+            $san = sprintf("UPDATE `userdata` SET `USERPASS`=1 WHERE `USERIP`='%s'", $USER_IP)
+            $Connection->query($san);
             header('refresh:0;url=https://'.USER_DOMAIN.'/index.html');
         }
     }
 } else {
     if(!$Row) {
         $USER_KEY = md5(random_bytes(32));
-        $Connection->query("INSERT INTO `userdata`(`USERIP`, `USERKEY`, `USERPASS`) VALUES ('$USER_IP','$USER_KEY',0)");
+        $san22 = sprintf("INSERT INTO `userdata`(`USERIP`, `USERKEY`, `USERPASS`) VALUES ('%s','%s',0)", $USER_IP, $USER_KEY)
+        $Connection->query($san22);
         die(json_encode(["solve"=>USER_FINISH]));
     } else {
         if($Row[2] == 1) {
